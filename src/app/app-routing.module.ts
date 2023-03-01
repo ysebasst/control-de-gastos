ac3fb593a41649877fd4098e87902e50fdc59738
Sebastian Sierra <ysebasst@gmail.com>
2023-03-01 17:12:10 -0500

    feat added initial config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      }
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
