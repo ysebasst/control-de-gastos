ed92535d6c18c99a4561316ec56c1df12ddfec48
Sebastian Sierra <yohan.sierra@farmatodo.com>
2023-03-03 18:24:44 -0500

    Added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAD6yn48nMtNhsn5w1FHENlVtgGgS8oBlQ",
+    authDomain: "control-gastos-b79e3.firebaseapp.com",
+    databaseURL: "https://control-gastos-b79e3-default-rtdb.firebaseio.com",
+    projectId: "control-gastos-b79e3",
+    storageBucket: "control-gastos-b79e3.appspot.com",
+    messagingSenderId: "197316633107",
+    appId: "1:197316633107:web:bf56ef7cc954dbe11d95e6",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAD6yn48nMtNhsn5w1FHENlVtgGgS8oBlQ",
+    authDomain: "control-gastos-b79e3.firebaseapp.com",
+    databaseURL: "https://control-gastos-b79e3-default-rtdb.firebaseio.com",
+    projectId: "control-gastos-b79e3",
+    storageBucket: "control-gastos-b79e3.appspot.com",
+    messagingSenderId: "197316633107",
+    appId: "1:197316633107:web:bf56ef7cc954dbe11d95e6",
+  },
 };
 
 /*
